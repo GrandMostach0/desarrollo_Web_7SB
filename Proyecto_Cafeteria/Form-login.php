ad756eac952ba8d44bd0125cd6674bc620f0f9d2
GrandMostach0 <Kreedlegend0@gmail.com>
2023-06-08 22:59:06 -0600

    agregando la funcionalidad de los hipervinculos del footer

--- Proyecto_Cafeteria/Form-login.php
+++ Proyecto_Cafeteria/Form-login.php
@@ -41,16 +41,16 @@
 
             <div class="informacion">
                 <div class="icons">
-                    <img class="facebook-f" src="icons/facebook-f.svg" alt="facebook-f">
-                    <img class="instagram" src="icons/instagram.svg" alt="instagram">
+                    <a target="_blank" href="https://www.facebook.com/PlaceryDelirio"><img class="facebook-f" src="icons/facebook-f.svg" alt="facebook-f"></a>
+                    <a target="_blank" href="https://www.instagram.com/placerydelirio/"><img class="instagram" src="icons/instagram.svg" alt="instagram"></a>
                 </div>
 
                 <p>© 2023, <a href="https://ecohete.com/" target="_blank">Placer & DelirioImpulsado por 🚀 eCohete | Agencia de eCommerce.</a></p>
             </div>
 
-            <p>Correo: <a href="">https://placerydelirio.com/</a></p>
+            <p>Pagina Secundario: <a href="https://placerydelirio.com">https://placerydelirio.com/</a></p>
 
-            <p>Tel: <a href="">+16508637580</a></p>
+            <p>Tel: <a href="tel:+16508637580">+16508637580</a></p>
         </div>
 
     <!-- VALIDACION DE INPUTS -->
